7b124d6db4398ee829c949bb6d6bfb921a38d956
Muntasirul Haque <fmuntasirhq@gmail.com>
2026-06-21 23:10:43 +0600

    boom

---
+++ self/day01/t1/src/StudentTester.java
@@ -0,0 +1,7 @@
+public class StudentTester {
+    public static void main (String [] args){
+        student std1= new student();
+      student std2= new student();
+        System.out.println(std1.name);
+    }
+}
---
+++ self/day01/t1/src/student.java
@@ -0,0 +1,7 @@
+public class student {
+    public class student{
+        public String name;
+        public int id;
+        public double cgpa;
+    }
+}
