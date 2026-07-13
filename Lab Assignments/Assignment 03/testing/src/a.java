af4bd6ef3ba998aac992885e82d495426c382218
Muntasirul Haque <fmuntasirhq@gmail.com>
2026-07-13 20:47:45 +0600

    yo

---
+++ Lab Assignments/Assignment 03/testing/src/a.java
@@ -0,0 +1,11 @@
+public class a {
+    public int x=4;
+    public int y=2;
+    public int z=5;
+    public void in(){
+        System.out.println(x+" "+x+y+" "+z); //4 42 5
+        System.out.println(x+y); // 6
+        System.out.println(x+y+""); // 6
+        System.out.println(z+" "+x+y+" "+z); //5 42 5
+    }
+}
---
+++ Lab Assignments/Assignment 03/testing/src/test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main (String[] args){
+        a a1= new a();
+        a1.in();
+    }
+}
