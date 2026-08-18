47d98850d23871ad55eb319dba265b7529db0b4b
Muntasirul Haque <fmuntasirhq@gmail.com>
2026-08-18 06:30:19 +0600

    yap

---
+++ Lab Assignments/Assignment 06/Task 06/src/tester.java
@@ -0,0 +1,9 @@
+public class tester {
+    public static void main(String[] args){
+        Trace trace1 = new Trace();
+        Trace trace2 = new Trace(trace1);
+        trace1.methodA(3, 2);
+        Trace.methodC(trace1, trace2);
+        trace2.methodB(1, 2);
+    }
+}
