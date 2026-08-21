5f2f10c47ada925c633e052bd9bef48ddf9130ba
Muntasirul Haque <fmuntasirhq@gmail.com>
2026-08-21 09:23:42 +0600

    hmm

---
+++ Lab Assignments/Assignment 07/Task 06/src/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main (String [] args){
+        B b1 = new B();
+        b1.methodB(3, 5);
+        A a1 = new A();
+        a1.methodA(12, -9);
+    }
+}
